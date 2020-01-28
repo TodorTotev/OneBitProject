dacade7c2698180513ba3c478782e17be8c46c09
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-01-28 13:18:09 +0200

    Added Common class library

---
+++ OneBitProject.Common/GlobalConstants.cs
@@ -0,0 +1,8 @@
+﻿// ReSharper disable InconsistentNaming
+namespace OneBitProject.Common
+{
+    public class GlobalConstants
+    {
+        public const string EntityAlreadyDeletedMessage = "Entity is already deleted";
+    }
+}
