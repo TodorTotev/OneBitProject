67c84a726ae2654960a0906b473916ad19850d0e
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-01-28 15:07:49 +0200

    Added href for material icons @ layout

--- OneBitProject.Web/ClientApp/src/components/Layout.js
+++ OneBitProject.Web/ClientApp/src/components/Layout.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import Header from './Header';
+import React, { Component } from "react";
+import { Container } from "reactstrap";
+import Header from "./Header";
 
 export class Layout extends Component {
   static displayName = Layout.name;
 
-  render () {
+  render() {
     return (
       <div>
+        <head>
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/icon?family=Material+Icons'
+          />
+        </head>
         <Header />
-        <Container>
-          {this.props.children}
-        </Container>
+        <Container>{this.props.children}</Container>
       </div>
     );
   }
