e602758dffe0eb9a002b91705f2bd0c0243674ce
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-01-28 14:35:00 +0200

    Updated tests for Header component

--- OneBitProject.Web/ClientApp/src/components/Header/index.test.js
+++ OneBitProject.Web/ClientApp/src/components/Header/index.test.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Header from "../Header/"
+import React from "react";
+import renderer from "react-test-renderer";
+import Header from "../Header/";
 
-it('renders correctly', () => {
-    const component = renderer.create()
+it("renders correctly", () => {
+  const component = renderer.create(<Header/>);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot()
 });
