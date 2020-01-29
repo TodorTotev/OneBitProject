d7ae0925895955d5c97643ad9daf21035a470afe
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-01-29 10:08:51 +0200

    Updated CustomersController

--- OneBitProject.Web/Controllers/CustomersController.cs
+++ OneBitProject.Web/Controllers/CustomersController.cs
@@ -1,26 +1,57 @@
 namespace OneBitProject.Web.Controllers
 {
+    using System;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Mvc;
+    using OneBitProject.Application.Common.Models;
     using OneBitProject.Application.Customer.Commands.Create;
     using OneBitProject.Application.Customer.Queries.GetAll;
+    using OneBitProject.Application.Customer.Queries.GetById;
 
     public class CustomersController : BaseController
     {
         [HttpGet]
         public async Task<ActionResult<GetAllCustomersViewModel>> GetAll()
         {
-            var result = await this.Mediator.Send(new GetAllCustomersQuery());
-            return this.Ok(result);
+            try
+            {
+                var result = await this.Mediator.Send(new GetAllCustomersQuery());
+                return this.Ok(result);
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CustomerLookupModel>> Get(int id)
+        {
+            try
+            {
+                var result = await this.Mediator.Send(new GetCustomerByIdQuery { Id = id });
+                return this.Ok(result);
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest();
+            }
         }
 
         [HttpPost]
         public async Task<ActionResult> Create(CreateCustomerCommand command)
         {
-            await this.Mediator.Send(command);
+            try
+            {
+                await this.Mediator.Send(command);
 
-            return this.NoContent();
+                return this.NoContent();
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest();
+            }
         }
     }
 }
