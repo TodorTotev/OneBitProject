24a08dc4cd3683361d712184e9a9a3d96d492ad5
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-01-29 20:23:17 +0200

    Updated Homepage with relevant information

--- OneBitProject.Web/ClientApp/src/components/Home.js
+++ OneBitProject.Web/ClientApp/src/components/Home.js
@@ -1,27 +1,120 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Home extends Component {
   static displayName = Home.name;
 
-  render () {
+  render() {
     return (
       <div>
-        <br/>
-        <br/>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+        <h1>Welcome!</h1>
+        <p>This is Todor Totev's OneBit project assignment built with:</p>
         <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+          <li>
+            <a href='https://get.asp.net/'>ASP.NET Core</a> and{" "}
+            <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>
+              C#
+            </a>{" "}
+            for cross-platform server-side code
+          </li>
+          <li>
+            <a href='https://facebook.github.io/react/'>React</a> for
+            client-side code
+          </li>
+          <li>
+            <a href='http://https://material-ui.com/.com/'>Material UI</a> for
+            layout and styling
+          </li>
         </ul>
-        <p>To help you get started, we have also set up:</p>
+        <p>Used technologies for back-end:</p>
         <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
+          <li>
+            <strong>Entity Framework Core</strong>
+          </li>
+          <li>
+            <strong>Following the CQRS pattern using MediatR</strong>
+          </li>
+          <li>
+            <strong>Object to model mapping using AutoMapper</strong>
+          </li>
+          <li>
+            <strong>Moq, Shouldly and xUnit for TDD Development</strong>
+          </li>
         </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <p>Used technologies for front-end:</p>
+        <ul>
+          <li>
+            <strong>ReactJS</strong>
+          </li>
+          <li>
+            <strong>React Router for routing</strong>
+          </li>
+          <li>
+            <strong>Material UI/Icons for styling</strong>
+          </li>
+          <li>
+            <strong>Jest, React test renderer for TDD Development</strong>
+          </li>
+        </ul>
+        <p>Database creation method:</p>
+        <ul>
+          <li>
+            <strong>Code first:</strong> I have used the code-first method
+            because it's easier for me to write the models and edit them when
+            needed rather than configuring the database first. The relationships
+            between the Customer and Order entities were created using the
+            fluent API.
+          </li>
+        </ul>
+        <p>There are five base layers in the project</p>
+        <ul>
+          <li>
+            <strong>Domain Layer</strong>
+            <ul>
+              <li>
+                Contains all entities, enums, exceptions, types and logic
+                specific to the domain. The Entity Framework related classes are
+                abstract, and should be considered in the same light as .NET
+                Core. For testing, im using an InMemory provider.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <strong>Application Layer</strong>
+            <ul>
+              <li>
+                Contains all application logic. It is dependent on the domain
+                layer, but has no dependencies on any other layer or project.
+                This layer defines interfaces that are implemented by outside
+                layers.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <strong>Persistence layer</strong>
+            <ul>
+              <li>
+                Contains database context, all configurations, migrations and
+                data seed. It depends only on the application layer
+              </li>
+            </ul>
+          </li>
+          <li>
+            <strong>Presentation Layer</strong>
+            <ul>
+              <li>
+                Contains all presentation logic. There is client application
+                based on React. Presentation layer depends only on application
+                layer.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <strong>Common Layer</strong>
+            <ul>
+              <li>Contains all cross-cutting concerns.</li>
+            </ul>
+          </li>
+        </ul>
       </div>
     );
   }
