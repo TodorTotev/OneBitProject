a0bba69183dca4e0e04905bdf5e2f8f015b907cc
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-01-29 20:20:13 +0200

    Configured routes

--- OneBitProject.Web/ClientApp/src/App/index.js
+++ OneBitProject.Web/ClientApp/src/App/index.js
@@ -1,18 +1,35 @@
 import React from "react";
-import { Route } from "react-router";
+
+// Main
+import { Route, Switch } from "react-router";
 import { Layout } from "../components/Layout";
 import { Home } from "../components/Home";
+
+// Customers
 import Customers from "../components/Customers/";
 import CreateCustomer from "../components/Customers/CreateForm";
 import EditCustomer from "../components/Customers/EditForm";
 
+// Orders
+import Orders from "../components/Orders";
+import CreateOrder from "../components/Orders/CreateForm"
+import EditOrder from "../components/Orders/EditForm"
+
+import NotFoundPage from "../components/NotFound";
+
 const App = () => {
   return (
     <Layout>
+      <Switch>
       <Route exact path='/' component={Home} />
       <Route exact path='/customers' component={Customers} />
       <Route exact path='/createCustomer' component={CreateCustomer} />
       <Route exact path='/editCustomer/:id' component={EditCustomer} />
+      <Route exact path='/orders/:id' component={Orders} />
+      <Route exact path='/createOrder/:id' component={CreateOrder}/>
+      <Route exact path='/editOrder/:id' component={EditOrder}/>
+      <Route component={NotFoundPage}/>
+      </Switch>
     </Layout>
   );
 };
